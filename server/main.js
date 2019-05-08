a588e67fd134f003069e2d7cb3fb811573ddbe11
Khong Minh Quan <khong.minh.quan@framgia.com>
2019-05-08 17:56:13 +0700

    Update

--- server/main.js
+++ server/main.js
@@ -4,16 +4,27 @@ import Tfidf from '../lib/tfidf'
 
 const tfidf = new Tfidf()
 
-
 function run() {
-    const files = fs.readdirSync(
-        './books/formated'
-    )
+    const files = fs.readdirSync('./books/formated')
+
     // eslint-disable-next-line no-restricted-syntax
     for (const file of files) {
         const filePath = path.join('./books/formated', file)
         tfidf.addDocumentFile(filePath)
     }
+
+    fs.writeFileSync(
+        './books/data/count-word.txt',
+        JSON.stringify(tfidf.showDocuments()),
+        (err) => {
+            if (err) {
+                // eslint-disable-next-line no-console
+                console.error(err)
+            }
+        }
+    )
+
+    tfidf.run('')
 }
 
 run()
